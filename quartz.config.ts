fbe1dea2943807370ea52a1b9df3ae5f5f117c0e
Whekau <109697540+Whekau@users.noreply.github.com>
2024-10-14 12:43:50 +0200

    Quartz sync: Oct 14, 2024, 12:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Back Home",
+    pageTitle: "Startseite",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: false,
