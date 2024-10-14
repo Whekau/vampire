4714ff5b8a993fcc78f0e8496b6b67ed57736f66
Whekau <109697540+Whekau@users.noreply.github.com>
2024-10-14 12:30:33 +0200

    Quartz sync: Oct 14, 2024, 12:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "Home",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
