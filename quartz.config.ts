838aeed9f712c34c6a881aff4b960052277c0f09
Whekau <109697540+Whekau@users.noreply.github.com>
2024-10-14 16:45:49 +0200

    Quartz sync: Oct 14, 2024, 4:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Home",
-    pageTitleSuffix: "Ongoing Vampire PnP Campaign",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
