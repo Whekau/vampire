20f0c17e5611abc88381a8da65c75af6eb44afe1
Whekau <109697540+Whekau@users.noreply.github.com>
2024-10-23 02:12:27 +0200

    Quartz sync: Oct 23, 2024, 2:12 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,7 +32,9 @@ afterBody: [
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({
+	rootName: "Vampire",
+	}),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -55,7 +57,9 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs({
+	rootName: "Vampire",
+	}), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
